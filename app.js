4c87c5253bfcc8047d6886c6832ba00cfbbb1168
Hatchwald <fatkhur139wec@gmail.com>
2024-03-08 19:49:56 +0700

    fix: validation and set cors

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const { sequelize } = require('./models');
 const app = express();
 const port = 3000;
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
--- controllers/invoiceController.js
+++ controllers/invoiceController.js
@@ -10,14 +10,28 @@ const getAllInvoices = async (req, res) => {
     }
 };
 
-const createInvoice = async (req, res) => {
+const createInvoice = async (req, res, next) => {
     const { invoice_no, date, customer, salesperson, payment_type, notes } = req.body;
+    console.log(req.body)
+    const requiredField = ['date', 'customer', 'salesperson'];
     try {
+        requiredField.forEach(field => {
+            if (!req.body[field]) {
+                const responseError = {
+                    type: 'Error',
+                    message: `${field} cannot be empty`,
+                    code: 400,
+                };
+
+                let error = new Error();
+                error = { ...error, ...responseError };
+                throw (error);
+            }
+        })
         const newInvoice = await Invoice.create({ invoice_no, date, customer, salesperson, payment_type, notes });
         res.status(201).json(newInvoice);
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        res.status(500).json({ error: error });
     }
 };
 

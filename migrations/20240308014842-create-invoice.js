072eac15106dcff195441efd14569ff445febdb5
Hatchwald <fatkhur139wec@gmail.com>
2024-03-08 08:53:51 +0700

    feat: add invoice fix: update the config

---
+++ migrations/20240308014842-create-invoice.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Invoices', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      invoice_no: {
+        type: Sequelize.STRING
+      },
+      date: {
+        type: Sequelize.DATE
+      },
+      customer: {
+        type: Sequelize.STRING
+      },
+      salesperson: {
+        type: Sequelize.STRING
+      },
+      payment_type: {
+        type: Sequelize.STRING
+      },
+      notes: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Invoices');
+  }
+};
